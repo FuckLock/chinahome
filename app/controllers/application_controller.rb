05581120e3925a971537948d54679cf80686fe5f
baodongdong <baodongdong@tigerjoys.com>
2017-11-21 18:43:59 +0800

    locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :turbolinks_app?
+  before_action :set_active_menu
 
   def turbolinks_app?
     @turbolinks_ap ||= request.user_agent.to_s.include?("turbolinks-app")
   end
+
+  def set_active_menu
+    @current = case controller_name
+               when  "pages"
+                 ["/wiki"]
+               else
+                 ["/#{controller_name}"]
+               end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  EMPTY_STRING = "".freeze
+
+  def render_list_items(list = [])
+    yield(list) if block_given?
+    items = []
+    list.each do |link|
+      item_class = EMPTY_STRING
+      urls = link.match(/href=(["'])(.*?)(\1)/) || []
+      url  = urls.length > 2 ? urls[2] : nextWhenVisible
+      if url && current_page?(url) || (@current && @current.include?(url))
+        item_class = "active"
+      end
+      items << content_tag("li", raw(link), class: item_class)
+    end
+    raw items.join(EMPTY_STRING)
+  end
+
 end
