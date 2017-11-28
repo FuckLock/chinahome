b67d5a14844b6ba09f4f9e8cb11030c238f8d784
baodongdong <1214063293@qq.com>
2017-11-28 22:21:43 +0800

    add register validation prompting

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -5,4 +5,13 @@ class AccountController < Devise::RegistrationsController
     super
   end
 
+  def create
+    build_resource(sign_up_params)
+    resource.login = params[resource_name][:login]
+    resource.email = params[resource_name][:email]
+    if resource.save
+      sign_in(resource_name, resource)
+    end
+  end
+
 end
--- db/migrate/20171127065634_devise_create_users.rb
+++ db/migrate/20171127065634_devise_create_users.rb
@@ -1,7 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string   'login',                                  null: false
+        t.string   'login',                                  null: false
         t.string   'name',                                   null: false
         t.string   'email',                                  null: false
         t.string   'email_md5',                              null: false
